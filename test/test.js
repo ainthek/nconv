cfc6572b5b2f0373dfe247aa9bc1659a0eb4be8c
Martin Marko <marcus@gratex.com>
2015-02-18 23:41:51 +0100

    unicde foot and inch marks now supported, not only apos

--- test/test.js
+++ test/test.js
@@ -3,37 +3,50 @@ var assert = require("./assert.js"); //extended node/assert library
 
 describe("f2cm", function() {
 	it("feet and inches", function() {
-		assert.equal(ft2cm("6'1''"), 185.42); //TODO: make test to comare with some other calculator
+		assert.equal(ft2cm("6'1''"), 185.42); 
 	});
 	it("feet", function() {
-		assert.equal(ft2cm("1'"), 30.48); //TODO: make test to comare with some other calculator
+		assert.equal(ft2cm("1'"), 30.48); 
 	});
 	it("inches", function() {
-		assert.equal(ft2cm("1''"), 2.54); //TODO: make test to comare with some other calculator
+		assert.equal(ft2cm("1''"), 2.54); 
 	});
 	//....
 	//....
 
 	it("detect and replace in string", function() {
-		assert.equal(ft2cm("I'm 5'2'' high"), "I'm 157.48 high"); //TODO: make test to comare with some other calculator
+		assert.equal(ft2cm("I'm 5'2'' high"), "I'm 157.48 high"); 
 	});
 
 	it("fwo times", function() {
-		assert.equal(ft2cm("6'1'' and 6'1''"), "185.42 and 185.42"); //TODO: make test to comare with some other calculator
+		assert.equal(ft2cm("6'1'' and 6'1''"), "185.42 and 185.42"); 
 	});
 	it("feet space inches", function() {
-		assert.equal(ft2cm("6' 1''"), "182.88 2.54"); //TODO: make test to comare with some other calculator
+		assert.equal(ft2cm("6' 1''"), "182.88 2.54"); 
 	});
 
 	it("toFixed", function() {
-		assert.equal(ft2cm("6'",0), "183"); //TODO: make test to comare with some other calculator
+		assert.equal(ft2cm("6'",0), "183"); 
 	});
 
 	it("add units", function() {
-		assert.equal(ft2cm("6'",0,"cm"), "183cm"); //TODO: make test to comare with some other calculator
+		assert.equal(ft2cm("6'",0,"cm"), "183cm"); 
 	});
 	it("add units", function() {
-		assert.equal(ft2cm("6'",2," cm"), "182.88 cm"); //TODO: make test to comare with some other calculator
+		assert.equal(ft2cm("6'",2," cm"), "182.88 cm"); 
 	});
+	//.......
+	it("feet - Foot and inch marks", function() {
+		assert.equal(ft2cm("6’"), "182.88"); 
+	});
+	it("inches - Foot and inch marks", function() {
+		assert.equal(ft2cm("1”"), "2.54"); 
+	});
+
+	it("feet and inches - Foot and inch marks", function() {
+		assert.equal(ft2cm("6’1”"), 185.42);  
+	});
+
+	
 	
 });
