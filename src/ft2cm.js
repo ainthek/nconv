116d294e9765ad9684d070241b3fc27c80bc340a
Martin Marko <marcus@gratex.com>
2015-02-18 22:46:50 +0100

    new failing test added

--- src/ft2cm.js
+++ src/ft2cm.js
@@ -1,5 +1,5 @@
 function ft2cm(ftString, fixed) {
-	return ftString.replace(/(?:(\d+)')?(?:(\d+)'')?/, function(m, f, i) {
+	return ftString.replace(/.*(?:(\d+)')?(?:(\d+)'')?.*/, function(m, f, i) {
 		console.log(f, i);
 		// rounding or toFixed or both ?
 		return ((f || 0) / 0.032808 + (i || 0) / 0.39370).toFixed(fixed != null ? fixed : 2);
--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
 var ft2cm = require("../src/ft2cm.js");
 var assert = require("./assert.js"); //extended node/assert library
 
-describe("f2cm shell process", function() {
+describe("f2cm", function() {
 	it("feet and inches", function() {
 		assert.equal(ft2cm("6'1''"), 185.42); //TODO: make test to comare with some other calculator
 	});
+	//....
+	//....
+
+	it("detect and replace in string", function() {
+		assert.equal(ft2cm("I'm 5'2'' high"), "I'm 157.48 high"); //TODO: make test to comare with some other calculator
+	});
+	
 });
