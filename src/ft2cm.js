f84c3deab259ad12506179acdff44622e3194a50
Marko Martin <marcus@gratex.com>
2015-03-22 08:56:20 +0100

    refactor for easier extensions

--- src/ft2cm.js
+++ src/ft2cm.js
@@ -1,9 +1,16 @@
+
+var re=/(?:(\d+)['’](\d+)(?:''|[”"]))|(?:(\d+)(?:''|[”"]))|(?:(\d+)['’])/g;
+
 function ft2cm(ftString, fixed, units) {
-	//’6”
-	return ftString.replace(/(?:(\d+)['’](\d+)(?:''|[”"]))|(?:(\d+)(?:''|[”"]))|(?:(\d+)['’])/g, function(m, f, i, ii, ff) {
-		//console.log(arguments);
-		// rounding or toFixed or both ?
-		return ((f || ff || 0) / 0.032808 + (i || ii || 0) / 0.39370).toFixed(fixed != null ? fixed : 2) + (units || "");
-	});
+
+    return ftString.replace(re, function(m, f, i, ii, ff) {
+        
+        var n1 = ((f || ff || 0) / 0.032808 + (i || ii || 0) / 0.39370);        
+        var n2 = n1.toFixed(fixed != null ? fixed : 2);
+        var formated = n2 + (units || "");
+
+        return formated;
+    });
+
 }
 module.exports = ft2cm;
